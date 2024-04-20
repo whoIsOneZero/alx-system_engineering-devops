3f452c4b6b8c4ef31d22bb1aef88d67e68e7ac5c
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-04-19 21:49:04 -0700

    add shebang

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install the `flask` package
-# particularly version 2.1.0
+#!/usr/bin/pup
+# install the `flask` package, version 2.1.0
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
