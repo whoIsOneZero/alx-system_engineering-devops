7a196494849b9bb1264faabcc1ac5d9ebc717e70
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-07-03 12:32:09 -0700

    configure host according to requirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install the flask package, version 2.1.0
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Install the Flask package, version 2.1.0
+
+# manifests/install_flask.pp
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask_werkzeug':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show Flask | grep Version | grep 2.1.0 && /usr/bin/pip3 show Werkzeug | grep Version | grep 2.1.1',
+  require => Package['python3-pip'],
+}
+
